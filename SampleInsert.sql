d48f99bbb7f8167b55a58ab540fd16712c3881e2
Mingshan Wang <margartewm3@gmail.com>
2016-02-10 12:38:32 -0800

    added sample insert into Department table

---
+++ SampleInsert.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO Department(DepartmentName) VALUES ('Computer Science');
+INSERT INTO Department(DepartmentName) VALUES ('Visual Arts');
+INSERT INTO Department(DepartmentName) VALUES ('Music');
+INSERT INTO Department(DepartmentName) VALUES ('Bioengineering');
+
+
