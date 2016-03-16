5a66ea06ff1f9da5480fea5e394615c1ecdce6b3
Mingshan Wang <margartewm3@gmail.com>
2016-03-15 20:04:10 -0700

    professor trigger done

--- Trigger.sql
+++ Trigger.sql
@@ -49,3 +49,30 @@ FOR EACH ROW EXECUTE PROCEDURE enrollment_limit_check();
 -- A professor should not have multiple sections at the same time. For example, a professor that is scheduled to teach classes X and Y 
 -- should not have conflicting sections, mainly overlapping meetings. It is enough to check for the regular meetings (e.g., "LE"). 
 -- Extra credit is given for checking conflicts on the irregular meetings too.
+DROP Trigger IF EXISTS professor_time_conflict_trigger on Instructor;
+
+CREATE OR REPLACE FUNCTION professor_time_conflict_check() RETURNS TRIGGER AS $professor_time_conflict_check$
+DECLARE t RECORD;
+DECLARE tt RECORD;
+    BEGIN
+    FOR t in SELECT c.MeetingID from Instructor i,ClassMeeting c WHERE i.FacultyName = NEW.FacultyName AND i.SectionID = c.SectionID
+    LOOP
+	FOR tt in SELECT c.MeetingID from ClassMeeting c WHERE c.SectionID = NEW.SectionID 
+	LOOP
+		if ((Select Time from WeeklyMeeting WHERE MeetingID = t.MeetingID) = 
+		 (Select Time from WeeklyMeeting WHERE MeetingID = tt.MeetingID ))
+		then
+			if ((Select DayOfTheWeek from WeeklyMeeting WHERE MeetingID = t.MeetingID) LIKE concat('%',(Select DayOfTheWeek from WeeklyMeeting WHERE MeetingID = tt.MeetingID),'%',NULL))
+				then
+					Raise Exception 'time conflict';
+			end if;
+		end if;
+	END LOOP;
+    END LOOP;
+	RETURN NEW;
+    END;
+$professor_time_conflict_check$ LANGUAGE plpgsql;
+
+CREATE TRIGGER professor_time_conflict_trigger Before INSERT ON Instructor
+FOR EACH ROW EXECUTE PROCEDURE professor_time_conflict_check();
+INSERT INTO Instructor VALUES(2,'Taylor Swift');
